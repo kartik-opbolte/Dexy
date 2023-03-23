38dc6592b9ead1516d8121a68bea9ecf287cdc7e
kartik-opbolte <97972997+kartik-opbolte@users.noreply.github.com>
2023-03-23 21:52:37 +0530

    Update index.jsx
    Signed-off-by: kartik-opbolte <97972997+kartik-opbolte@users.noreply.github.com>

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,26 +10,26 @@ export default function Index() {
       <img width="500" src="/img/bck.png" />
       </div>
         <p className="animateHeader text-4xl font-extrabold text-center text-white">
-          Treo
+          Vegito
         </p>
         <p className="animateHeader text-white text-opacity-50 text-center mt-5">
-      Treo is a multipurpose bot for your discord server with Moderation, Fun, Giveaway, Economy, Utility etc... features. 
+      Vegito is a multipurpose bot for your discord server with Antinuke, Moderation, Fun, Giveaway, Utility etc... features. 
 	  </p>
         <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
   
-            <Link href={"https://discord.com/oauth2/authorize?client_id=1016392200516550736&permissions=2146958591&scope=bot%20applications.commands"}>
+            <Link href={"https://discord.com/api/oauth2/authorize?client_id=1041326801860964364&permissions=8&scope=bot%20applications.commands"}>
               <a
                 className={
                   "flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " 
                 }
               >
-          Invite Treo
+          Invite Vegito
               </a>
             </Link>
   <div className="py-10"></div>
-  <Link href={"https://top.gg/bot/1016392200516550736/vote"}>
+  <Link href={"https://top.gg/bot/1041326801860964364/vote"}>
   <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Vote Treo</a>
+Vote Vegito</a>
   </Link>
         </div>
 
@@ -47,8 +47,8 @@ Vote Treo</a>
           <div className="col-span-4">
             <p className="text-white text-4xl font-bold" dangerouslySetInnerHTML={{ __html:"Treo Bot" }} />
             <p className="text-white text-md font-medium text-gray-500/75" dangerouslySetInnerHTML={{ __html: "How about creating the best environment to relax yourself with your own emotions?" }}/>
-            <a href="https://discord.com/oauth2/authorize?client_id=1016392200516550736&permissions=2146958591&scope=bot%20applications.commands" className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
-              <i className="fab fa-discord mr-2" />Invite Treo
+            <a href="https://discord.com/api/oauth2/authorize?client_id=1041326801860964364&permissions=8&scope=bot%20applications.commands" className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
+              <i className="fab fa-discord mr-2" />Invite Vegito
             </a>
           </div>
 
@@ -57,13 +57,13 @@ Vote Treo</a>
               <div>
                 <div className="text-white">
                   <p className="text-xl font-semibold mt-5"><i className={`fa fa-cogs text-2xl text-amber-500`} /> Optimization</p>
-                  <p className="text-gray-500 line-clamp-4">With fast and customizable settings specific to your server, A gives you a musical pleasure to the fullest.</p>
+                  <p className="text-gray-500 line-clamp-4">With the Antinuke protection to your server And servers your all needs</p>
                 </div>
               </div>
               <div>
                 <div className="text-white">
                   <p className="text-xl font-semibold mt-5"><i className={`fa fa-universal-access text-2xl text-amber-500`} /> Universal</p>
-                  <p className="text-gray-500 line-clamp-4">Treo is a multipurpose bot for your discord server with Moderation, Fun, Giveaway, Economy, Utility etc... features.</p>
+                  <p className="text-gray-500 line-clamp-4">Vegito is a multipurpose bot for your discord server with Antinuke, Moderation, Fun, Giveaway, Utility etc... features.</p>
                 </div>
               </div>
               <div>
@@ -88,7 +88,7 @@ Vote Treo</a>
                 <div className="lg:mx-4 lg:p-8 py-4 lg:py-16 flex items-center justify-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
                   <img src="https://www.linkpicture.com/q/Capture_449.png"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
                   <p className="text-3xl text-white font-semibold">Best Your Bot</p>
-                  <p className="text-md text-white font-medium text-gray-500 line-clamp-5">Are you ready to throw the best parties with Treo, the best and most up-to-date bot?</p>
+                  <p className="text-md text-white font-medium text-gray-500 line-clamp-5">Are you ready to throw the best parties with Vegito, the best and most up-to-date bot?</p>
                 </div>
                 <img src="https://www.linkpicture.com/q/Capture_449.png" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
               </>
