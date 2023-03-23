7ef5745534b3ca8fdaba42d285c9df74f9b9656b
kartik-opbolte <97972997+kartik-opbolte@users.noreply.github.com>
2023-03-23 22:00:02 +0530

    Update invite.jsx
    Signed-off-by: kartik-opbolte <97972997+kartik-opbolte@users.noreply.github.com>

--- pages/invite.jsx
+++ pages/invite.jsx
@@ -7,7 +7,7 @@ export default function invite() {
 }
 
 export async function getServerSideProps({ res }) {
-  res.writeHead(302, { Location: 'https://discord.com/oauth2/authorize?client_id=1016392200516550736&permissions=2146958591&scope=bot%20applications.commands' });
+  res.writeHead(302, { Location: 'https://discord.com/api/oauth2/authorize?client_id=1041326801860964364&permissions=8&scope=bot%20applications.commands' });
   res.end();
 
   return {
