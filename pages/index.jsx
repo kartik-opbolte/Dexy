17160d36fb4a662d3d1a4a143c341865a386fb8d
kartik-opbolte <97972997+kartik-opbolte@users.noreply.github.com>
2023-06-01 10:04:29 +0530

    Update index.jsx
    Signed-off-by: kartik-opbolte <97972997+kartik-opbolte@users.noreply.github.com>

--- pages/index.jsx
+++ pages/index.jsx
@@ -90,7 +90,7 @@ Vote Vegito</a>
                   <p className="text-3xl text-white font-semibold">Best Your Bot</p>
                   <p className="text-md text-white font-medium text-gray-500 line-clamp-5">Are you ready to throw the best parties with Vegito, the best and most up-to-date bot?</p>
                 </div>
-                <img src"https://cdn.discordapp.com/attachments/1077447634064781443/1113686340811362374/Desktop-screenshot_1.png" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
+                <img src="https://cdn.discordapp.com/attachments/1077447634064781443/1113686340811362374/Desktop-screenshot_1.png" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
               </>
       
         </div>
