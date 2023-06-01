e51ae6d3e86c01546a0af2a1ef70036e945900b3
kartik-opbolte <97972997+kartik-opbolte@users.noreply.github.com>
2023-06-01 10:02:17 +0530

    Update index.jsx
    Signed-off-by: kartik-opbolte <97972997+kartik-opbolte@users.noreply.github.com>

--- pages/index.jsx
+++ pages/index.jsx
@@ -46,7 +46,7 @@ Vote Vegito</a>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-y-12 lg:gap-y-0 lg:gap-x-12 py-24">
           <div className="col-span-4">
             <p className="text-white text-4xl font-bold" dangerouslySetInnerHTML={{ __html:"Vegito Bot" }} />
-            <p className="text-white text-md font-medium text-gray-500/75" dangerouslySetInnerHTML={{ __html: "How about creating the best environment to relax yourself with your own emotions?" }}/>
+            <p className="text-white text-md font-medium text-gray-500/75" dangerouslySetInnerHTML={{ __html: "How  creaboutating the best environment to relax yourself with your own emotions?" }}/>
             <a href="https://discord.com/api/oauth2/authorize?client_id=1041326801860964364&permissions=8&scope=bot%20applications.commands" className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
               <i className="fab fa-discord mr-2" />Invite Vegito
             </a>
@@ -90,7 +90,7 @@ Vote Vegito</a>
                   <p className="text-3xl text-white font-semibold">Best Your Bot</p>
                   <p className="text-md text-white font-medium text-gray-500 line-clamp-5">Are you ready to throw the best parties with Vegito, the best and most up-to-date bot?</p>
                 </div>
-                <img src="https://media.discordapp.net/attachments/1088382484229668914/1088654301431345223/IMG_20230324_081319.jpg" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
+                <img src"https://cdn.discordapp.com/attachments/1077447634064781443/1113686340811362374/Desktop-screenshot_1.png" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
               </>
       
         </div>
